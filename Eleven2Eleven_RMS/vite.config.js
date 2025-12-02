5e011e1b227ad25f6ed401419f0d87d4defc09a4
23120066 <nhanthvo810909@gmail.com>
2025-12-02 21:01:43 +0700

    init shadcn

--- Eleven2Eleven_RMS/vite.config.js
+++ Eleven2Eleven_RMS/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,5 +11,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-
 })

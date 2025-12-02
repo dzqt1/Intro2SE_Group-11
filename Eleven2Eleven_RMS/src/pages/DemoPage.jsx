7f2f2d35aef6645f810202a26defc0f878c8e22c
23120066 <nhanthvo810909@gmail.com>
2025-12-02 21:29:46 +0700

    make demo

---
+++ Eleven2Eleven_RMS/src/pages/DemoPage.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+export default function DemoPage(){
+    return(
+        <>
+            <div className="p-4">
+                <p className="text-5xl font-bold mb-4">Demo Page</p>
+                <Button className="bg-blue-500 text-white hover:bg-blue-600 active:bg-blue-700 w-50">
+                    Click Me
+                </Button>
+            </div>
+        </>
+    )
+}

3a87d3a9293c0fc726e03c254d595ed01abbb5a5
23120066 <nhanthvo810909@gmail.com>
2025-12-03 10:29:45 +0700

    add react-router-dom

--- Eleven2Eleven_RMS/src/App.jsx
+++ Eleven2Eleven_RMS/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import DemoPage from './pages/DemoPage'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <DemoPage />
+      <Routes>
+        <Route path="/demo" element={<DemoPage />} />
+        <Route path="*" element={<div className="p-4">Welcome to Eleven2Eleven RMS!</div>} />
+      </Routes>
     </>
   )
 }

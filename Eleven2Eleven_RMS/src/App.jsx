85dab12a1c49c187639821b99b8b4beab23776c6
lehai <leminhhai2904@gmail.com>
2025-12-29 16:32:19 +0700

    fix: prevent sidebar from jumping

--- Eleven2Eleven_RMS/src/App.jsx
+++ Eleven2Eleven_RMS/src/App.jsx
@@ -23,20 +23,22 @@ function App() {
         
         <div className="flex flex-row h-screen">
           <Side_Navbar />
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/orders" element={<RequireAuth><Orders /></RequireAuth>} />
-            <Route path="/users" element={<RequireAuth><UserManagement /></RequireAuth>} />
-            <Route path="/menu" element={<RequireAuth><MenuManagement /></RequireAuth>} />
-            <Route path="/ingredients" element={<RequireAuth><IngredientManagement /></RequireAuth>} />
-            <Route path="/kitchen" element={<RequireAuth><Kitchen /></RequireAuth>} />
-            
-            {/* Route cho trang điền form */}
-            <Route path="/reservation" element={<RequireAuth><TableReservation /></RequireAuth>} />
+          <div className="flex-1 overflow-y-scroll">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/orders" element={<RequireAuth><Orders /></RequireAuth>} />
+              <Route path="/users" element={<RequireAuth><UserManagement /></RequireAuth>} />
+              <Route path="/menu" element={<RequireAuth><MenuManagement /></RequireAuth>} />
+              <Route path="/ingredients" element={<RequireAuth><IngredientManagement /></RequireAuth>} />
+              <Route path="/kitchen" element={<RequireAuth><Kitchen /></RequireAuth>} />
+              
+              {/* Route cho trang điền form */}
+              <Route path="/reservation" element={<RequireAuth><TableReservation /></RequireAuth>} />
 
-            {/* Route cho trang xem danh sách */}
-            <Route path="/table-info" element={<RequireAuth><TableInfo /></RequireAuth>} />
-          </Routes>
+              {/* Route cho trang xem danh sách */}
+              <Route path="/table-info" element={<RequireAuth><TableInfo /></RequireAuth>} />
+            </Routes>
+          </div>
         </div>
 
       </OrderProvider>
